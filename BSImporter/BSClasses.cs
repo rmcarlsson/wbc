a0ef279d0de559c24b76e2e061f57c89e62f168c
Mikael Carlsson <rmcarlsson@gmail.com>
2016-03-16 22:31:38 +0100

    * Added Beersmith types

---
+++ BSImporter/BSClasses.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSImporter
+{
+    public class BSGrainBill
+    {
+        public string FermentableName { set; get; }
+        public double AmountPercent { set; get; }
+    }
+
+    public class BSMashStep
+    {
+        public int Temperature { set; get; }
+        public int TimeMinutes { set; get; }
+    }
+}
+
+
+
